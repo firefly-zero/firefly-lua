060b06681ceb574070e2241bdd2e096d322db3d9
gram <git@orsinium.dev>
2025-10-19 10:02:51 +0200

    try mlua

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 #![no_std]
 #![no_main]
 use firefly_rust as ff;
+use mlua::prelude::*;
 
 #[unsafe(no_mangle)]
 extern "C" fn boot() {
-    // ...
+    run().ok().unwrap()
+}
+
+fn run() -> LuaResult<()> {
+    let lua = Lua::new();
+
+    let map_table = lua.create_table()?;
+    map_table.set(1, "one")?;
+    map_table.set("two", 2)?;
+
+    lua.globals().set("map_table", map_table)?;
+
+    lua.load("for k,v in pairs(map_table) do print(k,v) end")
+        .exec()?;
+
+    Ok(())
 }
 
 #[unsafe(no_mangle)]
@@ -14,15 +30,5 @@ extern "C" fn update() {
 
 #[unsafe(no_mangle)]
 extern "C" fn render() {
-    ff::clear_screen(ff::Color::White);
-    ff::draw_triangle(
-        ff::Point { x: 60, y: 10 },
-        ff::Point { x: 40, y: 40 },
-        ff::Point { x: 80, y: 40 },
-        ff::Style {
-            fill_color: ff::Color::LightGray,
-            stroke_color: ff::Color::DarkBlue,
-            stroke_width: 1,
-        },
-    );
+    // ...
 }
