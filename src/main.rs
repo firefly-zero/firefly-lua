726807cdd4731b04d8f8aba3fa2622c33a2a3f54
gram <git@orsinium.dev>
2025-10-19 15:51:34 +0200

    use a working piccolo fork

--- src/main.rs
+++ src/main.rs
@@ -14,49 +14,38 @@ extern "C" fn boot() {
 fn run() -> Result<(), Box<dyn core::error::Error>> {
     let source = r#"
 function foo (msg)
-    print('foo')
-    print(msg)
-    return 'yolo'
+    return msg
 end
 
 bar = 2 + 3
     "#;
 
-    // Instantiate the Lua instance
-    let mut lua = Lua::full();
+    let mut lua = Lua::empty();
+    lua.enter(|ctx| {
+        use piccolo::stdlib::*;
+        load_base(ctx);
+        load_coroutine(ctx);
+        load_math(ctx);
+        load_string(ctx);
+        // load_table(ctx);
+    });
 
-    // Enter a context
     let ex = lua.try_enter(|ctx| {
-        // Get the global env
         let env = ctx.globals();
-        // Run the lua script in the global context
         let closure = Closure::load_with_env(ctx, None, source.as_bytes(), env)?;
-        // Create an executor that will run the lua script
         let ex = Executor::start(ctx, closure.into(), ());
-
-        // Return the executor to ouside the scope. We must stash it to allow it to escape the scope.
         Ok(ctx.stash(ex))
     })?;
 
-    // Use the `execute` helper to execute the lua script
     lua.execute::<()>(&ex)?;
 
-    // Enter a new context
     let ex = lua.try_enter(|ctx| {
-        // Get the global env
         let env = ctx.globals();
-
-        // Get the function out of the lua context
         let foo = env.get::<_, Closure>(ctx, "foo")?;
-
-        // Create an executor to run it, and pass it our arguments list.
         let ex = Executor::start(ctx, foo.into(), "this is my message");
-
-        // Return it stashed so we can pull it out of the context
         Ok(ctx.stash(ex))
     })?;
 
-    // Get the return value
     let result = lua.execute::<String>(&ex)?;
     ff::log_debug(&result);
     Ok(())
